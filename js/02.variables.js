349dac8dab77ca9007e045a6c355b8518b33f7ec
CIDENET\jmejia <jmejia@cidenet.com.co>
2018-11-19 17:33:54 -0500

    first commit

---
+++ js/02.variables.js
@@ -0,0 +1,27 @@
+
+/*=============================================
+   // LAS VARIABLES            
+=============================================*/
+
+var numeroDecimal = 5;
+console.log(numeroDecimal);
+
+var palabra = "palabras";
+console.log("palabra",palabra);
+
+var colores = ["rojo","amarillo"]
+console.log(colores);
+
+var jugo = {"ingrediente1":"fresa",
+			"ingrediente2":"lulo",
+			"ingrediente3":"mango"}
+console.log("jugo",jugo.ingrediente1);
+var caja = document.querySelector("#caja");
+console.log("caja",caja);
+
+caja.style.width="200px";
+caja.style.height="200px";
+caja.style.background="red";
+
+var cajas = document.querySelectorAll(".cajas");
+console.log("cajas",cajas);
